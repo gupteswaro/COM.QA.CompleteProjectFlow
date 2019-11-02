599d87d2b42aab13acb3767d2bb85de26dd5bf98
ADMIN <ADMIN@ADMIN-PC>
2019-11-02 08:02:43 +0530

    Commited 2nd trial

---
+++ src/test/java/Project_Test1/Test3.java
@@ -0,0 +1,13 @@
+package Project_Test1;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void getMessagefromGupt1()
+	{
+		System.out.println("Hello guys accept my request1");
+	}
+
+
+}
